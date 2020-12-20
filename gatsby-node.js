16a009266818dae7b2dac9b95003b44c88d16a76
Moreno Ortega Oscar David <oscar.david.moreno.ortega@uabc.edu.mx>
2020-12-19 20:35:40 -0800

    79 completo

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,36 @@
  */
 
 // You can delete this file if you're not using it
+const path = require('path');
+const { paginate }  = require('gatsby-awesome-pagination');
+
+exports.createPages = async ({ actions, graphql })  => {
+    const { createPage } = actions;
+  
+    const posts = await graphql(`
+        query {
+            allStrapiPost ( sort: { fields: createdAt, order: DESC } ) {
+                nodes {
+                    id
+                    title
+                    url
+                    content
+                    createdAt
+                    miniature{
+                        publicURL
+                    }
+                }
+            }
+        }
+    `);
+  
+    // console.log(posts.data.allStrapiPost.nodes);
+    // Create your paginated pages
+    paginate({
+      createPage, // The Gatsby `createPage` function
+      items: posts.data.allStrapiPost.nodes, // An array of objects
+      itemsPerPage: 5, // How many items you want per page
+      pathPrefix: '/', // Creates pages like `/blog`, `/blog/2`, etc
+      component: path.resolve(`src/templates/blog.js`), // Just like `createPage()`
+    })
+  }
---
+++ src/templates/blog.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function blog(props) {
+    console.log(props);
+    return (
+        <div>
+            <h1> Estamos en el Blog </h1>
+        </div>
+    )
+}
